fa12f67d515b154b887c24476bed9ea81600466a
ACasagli13 <andrea.casagli@gmail.com>
2023-01-27 19:34:08 +0100

    Prova risoluzione conflitto

--- ProgettoDiTestGit/src/test/git/MainTest.java
+++ ProgettoDiTestGit/src/test/git/MainTest.java
@@ -4,7 +4,8 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		System.out.println("Hello Italy!");
+		System.out.println("I'm Emilia");
 	}
 
 }
